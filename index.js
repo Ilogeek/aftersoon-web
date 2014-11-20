62ecc816d4808f5220d123e25595af7bc08c71c9
Hugo <zilliox.hugo+git@gmail.com>
2014-11-20 17:08:15 +0100

    debug

--- index.js
+++ index.js
@@ -1,56 +1,9 @@
 var express = require('express');
 
 var http = require('http');
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 //var User = require(./model_user);
-
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    bcrypt = require('bcrypt'),
-    SALT_WORK_FACTOR = 10;
- 
-var UserSchema = new Schema({
-    //id: Schema.ObjectId,
-    //email: { type: String, required: true, index: { unique: true } },
-    //password: { type: String, required: true }
-    //nom_usuel: { type : String, match: /^[a-zA-Z0-9-_]+$/ , required : true},
-    telephone: String,
-    //adresse: { type:String, required: true},
-    gps: String
-});
- 
-UserSchema.pre(save, function(next) {
-    var user = this;
- 
-// only hash the password if it has been modified (or is new)
-if (!user.isModified('password')) return next();
- 
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
- 
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-        if (err) return next(err);
- 
-        // override the cleartext password with the hashed one
-        user.password = hash;
-        next();
-    });
-});
- 
- 
-});
- 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
-};
- 
-module.exports = mongoose.model(User, UserSchema);
 
 
 var app = express();
@@ -85,17 +38,11 @@ app.get('/hugo', function(request, response) {
 	response.send('Page')
 });
 
-/*app.get('/users', function(request, response){
+app.get('/users', function(request, response){
 
-  return User.find(function(err, users)) {
-    if(!err) {
-      return res.send(users);
-    } else {
-      return console.log(err);
-    }
-  });
+  console.log(':)');
 
-});*/
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
