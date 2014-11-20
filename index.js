6fb28b6e726fce4e60acbdf5fd811720a1f86493
Hugo <zilliox.hugo+git@gmail.com>
2014-11-20 17:45:36 +0100

    debug

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-/*var express = require('express');
+var express = require('express');
 
 var http = require('http');
 var mongoose = require('mongoose');
 
-//var User = require(./model_user);
-
-
 var app = express();
-
-var uristring = process.env.MONGOLAB_URI;
-var theport = process.env.PORT || 5000;
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-
-
 // connection to mongolab
-mongoose.connect(uristring, function (err, res) {
+mongoose.connect(process.env.MONGOLAB_URI, function (err, res) {
   if (err) {
-  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  console.log ('ERROR connecting to: ' + process.env.MONGOLAB_URI + '. ' + err);
   } else {
-  console.log ('Succeeded connected to: ' + uristring);
+  console.log ('Succeeded connected to: ' + process.env.MONGOLAB_URI);
   }
 });
 
@@ -36,45 +28,10 @@ app.get('/hugo', function(request, response) {
 });
 
 app.get('/users', function(request, response){
-
-  
-
+  response.send('users');
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-}) */
+}) 
 
-var application_root = __dirname,
-    express = require("express"),
-    path = require("path"),
-    mongoose = require('mongoose');
-
-var app = express.createServer();
-
-// Database
-mongoose.connect(process.env.MONGOLAB_URI, function (err, res) {
-  if (err) {
-  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-  console.log ('Succeeded connected to: ' + uristring);
-  }
-});
-
-// Config
-
-app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(application_root, "public")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.get('/api', function (req, res) {
-  res.send('Ecomm API is running');
-});
-
-// Launch server
-
-app.listen((process.env.PORT || 5000));
