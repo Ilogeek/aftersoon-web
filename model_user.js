e61f45d2aa917d0fcba9734a3739902854b676c4
Hugo <zilliox.hugo+git@gmail.com>
2014-11-20 17:53:19 +0100

    debug

--- model_user.js
+++ model_user.js
@@ -4,15 +4,16 @@ module.exports = function(mongoose) {
     bcrypt = require('bcrypt'),
     SALT_WORK_FACTOR = 10;*/
 
+var mongoose = require('mongoose';)
 var Schema = mongoose.Schema; 
 
 var UserSchema = new Schema({
-    id: Schema.ObjectId,
+    /*id: Schema.ObjectId,
     email: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true }
-    nom_usuel: { type : String, match: /^[a-zA-Z0-9-_]+$/ , required : true},
+    nom_usuel: { type : String, match: /^[a-zA-Z0-9-_]+$/ , required : true},*/
     telephone: String,
-    adresse: { type:String, required: true},
+    //adresse: { type:String, required: true},
     gps: String
 });
  
