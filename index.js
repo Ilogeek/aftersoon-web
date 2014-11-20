dfafee702519a212c269df7b6e1eef2af3b72e6f
Hugo <zilliox.hugo+git@gmail.com>
2014-11-20 23:15:36 +0100

    debug

--- index.js
+++ index.js
@@ -17,7 +17,18 @@ mongoose.connect(process.env.MONGOLAB_URI, function (err, res) {
   }
 });
 
-var UserModel = require(./model_user)(mongoose);
+var Schema = mongoose.Schema; 
+var UserSchema = new Schema({
+    /*id: Schema.ObjectId,
+    email: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true }
+    nom_usuel: { type : String, match: /^[a-zA-Z0-9-_]+$/ , required : true},*/
+    telephone: String,
+    //adresse: { type:String, required: true},
+    gps: String
+});
+
+var UserModel = mongoose.model('UserSchema', UserSchema);
 var Hugo = new models.UserModel({'telephone':'test'});
 
 // Root
