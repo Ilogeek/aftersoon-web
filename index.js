e0aa036af7d43005bae1e227ece37e030508dce7
Hugo <zilliox.hugo+git@gmail.com>
2014-11-20 17:40:45 +0100

    debug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 
 var http = require('http');
 var mongoose = require('mongoose');
@@ -14,25 +14,20 @@ var theport = process.env.PORT || 5000;
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-// connection test to mongodb
-var connected = "nop";
+
 
 // connection to mongolab
 mongoose.connect(uristring, function (err, res) {
   if (err) {
   console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  connected = err;
   } else {
   console.log ('Succeeded connected to: ' + uristring);
-  connected = "yup";
   }
 });
 
-var users = require('./user_model')(mongoose);
-
 // Root
 app.get('/', function(request, response) {
-  response.send('Hello '+ connected);
+  response.send('Hello');
 });
 
 // example
@@ -42,10 +37,39 @@ app.get('/hugo', function(request, response) {
 
 app.get('/users', function(request, response){
 
-  console.log(':)');
+  
 
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+}) */
+
+var application_root = __dirname,
+    express = require("express"),
+    path = require("path"),
+    mongoose = require('mongoose');
+
+var app = express.createServer();
+
+// Database
+
+mongoose.connect('mongodb://localhost/ecomm_database');
+
+// Config
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.get('/api', function (req, res) {
+  res.send('Ecomm API is running');
+});
+
+// Launch server
+
+app.listen(4242);
