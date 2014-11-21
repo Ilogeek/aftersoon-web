ee5ba5171e3bc90e28c6d18bae05f06fb53dcf2b
Hugo <zilliox.hugo+git@gmail.com>
2014-11-21 10:25:19 +0100

    debug

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGOLAB_URI, function (err, res) {
 });
 
 var UserModel = require('./model_user')(mongoose, bcrypt, SALT_WORK_FACTOR);//mongoose.model('UserSchema', UserSchema);
-var Hugo = new UserModel({'telephone':'test'});
+var Hugo = new UserModel({'password':'test','telephone':'test'});
 Hugo.cryptPassword('test');
 
 // Root
